3da38d861137d597919d9f869ccd3d929bdefd43
TheLazyDesigner <mathieu.croset@gmail.com>
2018-12-21 12:51:48 +0100

    Add files via upload

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -2,18 +2,17 @@ $(window).on('load', function() {});
 $(document).ready(function() {
 
 	var currentTime = new Date().getHours();
-	console.log(currentTime);
+	// console.log(currentTime);
 	if (17 <= currentTime || currentTime < 8) {
-		console.log('night time');
+		// console.log('night time');
 	} else {
-		console.log('daytime');
+		// console.log('daytime');
 		$('#layer-3, #layer-4, #layer-5').remove()
 	}
 
 	setInterval(function() {
 		$('.svg_lights_layer').css('opacity', Math.random());
 	}, 500)
-	// console.log($('#stars path'));
 	$('#stars path').each(function(index, val) {
 		setInterval(function() {
 			if (Math.random() > 0.5) {
